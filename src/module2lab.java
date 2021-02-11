f5e1b335bed318674944cd2eda58df81a49a2f5d
Heather Kusma <heatherkusma@heathers-air.quinnipiac.edu>
2021-02-10 21:29:51 -0500

    more println statements

--- src/module2lab.java
+++ src/module2lab.java
@@ -2,5 +2,7 @@
 public class module2lab {
 	public static void main (String []args) {
 		System.out.println("Git is confusing");
+		System.out.println("I'm really not a fan of it");
+		System.out.println("I keep forgetting to save my file before trying to commit it");
 	}
 }
