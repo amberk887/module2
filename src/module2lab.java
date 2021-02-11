9703d056c3fc32ea94e0ca85145b0c3d4c0fbf0f
Heather Kusma <heatherkusma@heathers-air.quinnipiac.edu>
2021-02-10 21:33:10 -0500

    2 more printlns

--- src/module2lab.java
+++ src/module2lab.java
@@ -4,5 +4,9 @@ public class module2lab {
 		System.out.println("Git is confusing");
 		System.out.println("I'm really not a fan of it");
 		System.out.println("I keep forgetting to save my file before trying to commit it");
+		System.out.println("This is my last commit thank goodness");
+		System.out.println("Oops forgot two commits had to have at least 2 lines");
+		
+		
 	}
 }
